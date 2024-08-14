fc9e204c329a3737568b54844fee6d4c03b69a54
user9261 <nre12270@gmail.com>
2024-08-14 18:40:44 +0900

    プロジェクト ファイルを追加します。

---
+++ DrawApp/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawApp
+{
+    public partial class Form1 : Form
+    {
+        Point startPos, endPos;
+        Pallet pallet;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.pallet = new Pallet();
+            pallet.Show();
+        }
+
+        private void MousePressed(object sender, MouseEventArgs e)
+        {
+            // 円の始点座標をstartPosに保存する
+            this.startPos = new Point(e.X, e.Y);
+
+        }
+
+        private void MouseDragged(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) //ドラッグしている場合
+            {
+                // 終点座標を更新する
+                this.endPos = new Point(e.X, e.Y);
+                Invalidate();
+            }
+
+        }
+
+        private void DrawFigures(object sender, PaintEventArgs e)
+        {
+            // パレット情報を参照する
+            int type = this.pallet.GetFigureType();
+            Color color = this.pallet.GetColor();
+            int penSize = this.pallet.GetPenSize();
+
+
+            if (type == 1) // 円を描画する
+            {
+                SolidBrush brush = new SolidBrush(color);
+
+                int width = this.endPos.X - this.startPos.X;
+                int height = this.endPos.Y - this.startPos.Y;
+                e.Graphics.FillEllipse(brush,
+                    this.startPos.X, this.startPos.Y, width, height);
+            }
+            else if (type == 2) // 長方形を描画する
+            {
+                SolidBrush brush = new SolidBrush(color);
+
+                int width = this.endPos.X - this.startPos.X;
+                int height = this.endPos.Y - this.startPos.Y;
+                e.Graphics.FillRectangle(brush, this.startPos.X,
+                    this.startPos.Y, width, height);
+            }
+            else if (type == 3) // 直線を描画する
+            {
+                Pen p = new Pen(color, penSize);
+                e.Graphics.DrawLine(p, this.startPos.X, this.startPos.Y,
+                    this.endPos.X, this.endPos.Y);
+            }
+        
+
+    }
+}
+}
---
+++ DrawApp/Pallet.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DrawApp
+{
+    partial class Pallet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.penSizeBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(252, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 156);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Rectangle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.RectButtonClicked);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(58, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 156);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Circle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.CircleButtonClicked);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button3.Location = new System.Drawing.Point(464, 54);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 156);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Line";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.LineButtonClicked);
+            // 
+            // colorButton
+            // 
+            this.colorButton.BackColor = System.Drawing.SystemColors.WindowText;
+            this.colorButton.Location = new System.Drawing.Point(690, 103);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(61, 58);
+            this.colorButton.TabIndex = 1;
+            this.colorButton.UseVisualStyleBackColor = false;
+            this.colorButton.Click += new System.EventHandler(this.ColorButtonClicked);
+            // 
+            // penSizeBox
+            // 
+            this.penSizeBox.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.penSizeBox.Location = new System.Drawing.Point(667, 192);
+            this.penSizeBox.Name = "penSizeBox";
+            this.penSizeBox.Size = new System.Drawing.Size(111, 29);
+            this.penSizeBox.TabIndex = 2;
+            this.penSizeBox.Text = "3";
+            // 
+            // Pallet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 253);
+            this.Controls.Add(this.penSizeBox);
+            this.Controls.Add(this.colorButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Pallet";
+            this.Text = "Pallet";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.TextBox penSizeBox;
+    }
+}
